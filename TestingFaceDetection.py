6e74fc2e50f32a170a3fcfa63a2d27c91da54684
Noam Tirosh <noamiko.tirosh7@gmail.com>
2022-02-15 14:54:44 +0200

    Downloaded the cascade xml file and documented and fixed the TestingFaceDetection.py a little bit.

--- TestingFaceDetection.py
+++ TestingFaceDetection.py
@@ -1,28 +1,47 @@
 import cv2
 
 # Load the cascade
+# The cascade xml file is a set of input data that allows to detect faces in pictures
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# To capture video from webcam.
+# Capturing video from the webcam
 cap = cv2.VideoCapture(0)
-# To use a video file as input
+
+# Using a video file instead:
 # cap = cv2.VideoCapture('filename.mp4')
+
+# Creating the video file to which the stream is being recorded
+fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+VideoWriter = cv2.VideoWriter('C:/Users/User/PycharmProjects/NoamCamCodeFromGit/video.avi', fourcc, 10.0, (640, 480))
+
 
 while True:
     # Read the frame
     _, img = cap.read()
-    # Convert to grayscale
+
+    # Converting to grayscale (The face detection only works with pictures in grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # Detect the faces
+
+    # Detecting faces in the frame
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    # Draw the rectangle around each face
+
+    # Drawing a rectangle around detected faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    # Display
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 0), 2)
+
+    # Displaying the frame
     cv2.imshow('img', img)
-    # Stop if escape key is pressed
+
+    # Saving the frame to the video
+    VideoWriter.write(img)
+
+    # If the ESC key is pressed, the program stops
     k = cv2.waitKey(30) & 0xff
     if k==27:
         break
+
 # Release the VideoCapture object
 cap.release()
+
+# Release the VideoWriter object
+VideoWriter.release()
