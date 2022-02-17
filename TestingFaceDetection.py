a50ad0505601a251e1d79c5e03cd4c7abdb7bf0d
Noam Tirosh <noamiko.tirosh7@gmail.com>
2022-02-17 11:08:54 +0200

    Added the feature that saves the pictures of the faces in a folder

--- TestingFaceDetection.py
+++ TestingFaceDetection.py
@@ -1,3 +1,5 @@
+import os
+
 import cv2
 
 # Load the cascade
@@ -12,22 +14,34 @@ cap = cv2.VideoCapture(0)
 
 # Creating the video file to which the stream is being recorded
 fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
-VideoWriter = cv2.VideoWriter('C:/Users/User/PycharmProjects/NoamCamCodeFromGit/video.avi', fourcc, 10.0, (640, 480))
+VideoWriter = cv2.VideoWriter('E:/NoamCam/video.avi', fourcc, 10.0, (640, 480))
 
+datasets = 'datasets'
+sub_data = 'Noam'
+
+count = 0
+
+path = os.path.join(datasets, sub_data)
+if not os.path.isdir(path):
+    os.mkdir(path)
 
 while True:
     # Read the frame
     _, img = cap.read()
 
-    # Converting to grayscale (The face detection only works with pictures in grayscale
+    # Converting to gray scale (The face detection only works with pictures in gray scale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Detecting faces in the frame
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-    # Drawing a rectangle around detected faces
+    # Drawing a rectangle around detected faces, and capturing a picture of the face
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 0), 2)
+        cv2.rectangle(img, (x, y), (x+w, y+h), (50, 50, 250), 2)
+        face = gray[y:y + h, x:x + w]
+        face_resize = cv2.resize(face, (500, 500))
+        cv2.imwrite('% s/% s.png' % (path, count), face_resize)
+        count += 1
 
     # Displaying the frame
     cv2.imshow('img', img)
@@ -37,7 +51,7 @@ while True:
 
     # If the ESC key is pressed, the program stops
     k = cv2.waitKey(30) & 0xff
-    if k==27:
+    if k == 27:
         break
 
 # Release the VideoCapture object
