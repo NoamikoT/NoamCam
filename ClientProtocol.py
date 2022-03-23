b66d1adefe0b5855af135fe98469d47ccc51e35e
Noam Tirosh <noamiko.tirosh7@gmail.com>
2022-03-23 15:06:38 +0200

    Created Client Protocol and built important functions, need to finish the unpacked() function in both.

--- ClientProtocol.py
+++ ClientProtocol.py
@@ -1,4 +1,5 @@
-
+import os
+import PIL
 
 def unpack(message):
 
@@ -77,3 +78,52 @@ def build_turn_off():
     code = "040"
 
     return code
+
+
+def build_frame_send(frame_path):
+
+    """
+    Building a message containing a frame of the video
+    :param frame_path: The path to the frame
+    :type frame_path: String
+    :return: The packed message ready to send
+    :rtype: String
+    """
+
+    with open(frame_path, 'rb') as f:
+        frame = f.read()
+        f.close()
+
+    frame_length = os.path.getsize(frame_path)
+
+    frame_length = frame_length.zfill(10)
+
+    code = "001"
+
+    packed_message = code + frame_length + frame
+
+    return packed_message
+
+
+def build_photo_send(photo_path):
+    """
+    Building a message containing a photo caught by face recognition
+    :param photo_path: The path to the photo
+    :type photo_path: String
+    :return: The packed message ready to send
+    :rtype: String
+    """
+
+    with open(photo_path, 'rb') as f:
+        photo = f.read()
+        f.close()
+
+    photo_length = os.path.getsize(photo_path)
+
+    photo_length= photo_length.zfill(10)
+
+    code = "001"
+
+    packed_message = code + photo_length + photo
+
+    return packed_message
