099a0de99a5dbee4f715e3376cfe3db31f954bf4
Noam Tirosh <noamiko.tirosh7@gmail.com>
2022-04-21 13:13:46 +0300

    Working general main screen with id's

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -13,3 +13,13 @@ A
 ServerLogic.py,6\a\6a58c195cc9cdad19d7b82077379b22a6d85ae02
 j
 :.idea/dataSources/487acec4-5ff8-4c20-922e-3acb85b35e97.xml,4\f\4fe66fcfca38149ff74f8ed90aa98026b26b5679
+A
+ClientProtocol.py,4\3\437cb85c1158219430fdd0c872f4e8076272509f
+>
+ClientComms.py,8\4\84d89e710e78d48eb411c8b852bacdb931ea70d3
+G
+TestingFaceDetection.py,7\9\79833b8f0b30c4f24d44831f8cf29184709c8493
+;
+DB_Class.py,e\4\e4e54efb5496cefef9075953a0db8cf9463ff235
+>
+ServerComms.py,f\a\fa9ad3d924f9f395bf5fd30fc61e22d06d0a68ac
--- CameraPanelTest.py
+++ CameraPanelTest.py
@@ -1,43 +1,47 @@
 import wx
+import DB_Class
 
-########################################################################
+
 class CameraPanel(wx.Panel):
     """"""
 
     # ----------------------------------------------------------------------
-    def __init__(self, parent, id):
-        wx.Panel.__init__(self, parent, id)
+    def __init__(self, parent, start_x, start_y, id_number):
+        wx.Panel.__init__(self, parent, pos=(start_x, start_y), size=(347, 267))
+
+        self.start_x = start_x
+        self.start_y = start_y
+        self.id_number = id_number
 
         self.SetBackgroundColour("white")
         self.Bind(wx.EVT_PAINT, self.OnPaint)
 
-        self.settings_frame = SettingsFrame()
+        self.settings_frame = SettingsFrame(self.id_number)
 
-        alert = wx.Button(self, label='Alert', pos=(100, 200))
-        alert.Bind(wx.EVT_BUTTON, self.alert_call)
+        self.alert = wx.Button(self, label='Alert', pos=(50, 205))
+        self.alert.Bind(wx.EVT_BUTTON, self.alert_call)
 
-        face = wx.ToggleButton(self, label='Face', pos=(100, 230))
-        face.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_face_detection)
+        self.face = wx.ToggleButton(self, label='Face', pos=(50, 235))
+        self.face.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_face_detection)
 
-        zoom = wx.Button(self, label='zoom', pos=(200, 200))
-        zoom.Bind(wx.EVT_BUTTON, self.toggle_face_detection)
+        self.zoom = wx.Button(self, label='zoom', pos=(200, 205))
+        self.zoom.Bind(wx.EVT_BUTTON, self.call_zoom_screen)
 
-        settings = wx.Button(self, label='Settings', pos=(200, 230))
+        settings = wx.Button(self, label='Settings', pos=(200, 235))
         settings.Bind(wx.EVT_BUTTON, self.settings_screen)
 
     def alert_call(self, e):
         print("Called alert")
 
     def toggle_face_detection(self, e):
-        obj = e.GetEventObject()
-        is_pressed = obj.GetValue()
+        is_pressed = self.face.GetValue()
         if is_pressed:
             print("Face detection is on")
         else:
             print("Face detection is off")
 
     def call_zoom_screen(self, e):
-        print("Called zoom")
+        print("Called zoom on panel " + str(self.id_number))
 
     def settings_screen(self, e):
         if not self.settings_frame.IsShown():
@@ -46,8 +50,6 @@ class CameraPanel(wx.Panel):
         else:
             self.settings_frame.Hide()
 
-
-
     def OnPaint(self, event):
         """set up the device context (DC) for painting"""
         dc = wx.PaintDC(self)
@@ -55,54 +57,140 @@ class CameraPanel(wx.Panel):
         # Black filled rectangle
         dc.SetPen(wx.Pen("black"))
         dc.SetBrush(wx.Brush("black"))
-        dc.DrawRectangle(220, 10, 200, 200)
-
+        dc.DrawRectangle(0, 0, 347, 197)
 
         # Red filled rectangle
         dc.SetPen(wx.Pen("red"))
         dc.SetBrush(wx.Brush("red"))
-        dc.DrawRectangle(220, 210, 200, 100)
+        dc.DrawRectangle(0, 0 + 197, 347, 70)
+
 
 class MainFrame(wx.Frame):
 
     # ----------------------------------------------------------------------
-    def __init__(self):
+    def __init__(self, start_x, start_y):
         """Constructor"""
 
-        camera_window_placement = [[]]
+        super().__init__(None, size=(1900, 1000), title="Main Screen")
 
-        wx.Frame.__init__(self, None, size=(1900, 1000), title="Main Screen")
-        panel = CameraPanel(self, -1)
+        self.
+
+        # First row
+        first_panel = CameraPanel(self, start_x, start_y, 1)
+        second_panel = CameraPanel(self, start_x + 349, start_y, 2)
+        third_panel = CameraPanel(self, start_x + 698, start_y, 3)
+
+        # Second row
+        forth_panel = CameraPanel(self, start_x, start_y + 269, 4)
+        fifth_panel = CameraPanel(self, start_x + 349, start_y + 269, 5)
+        sixth_panel = CameraPanel(self, start_x + 698, start_y + 269, 6)
+
+        # Third row
+        seventh_panel = CameraPanel(self, start_x, start_y + 538, 7)
+        eighth_panel = CameraPanel(self, start_x + 349, start_y + 538, 8)
+        ninth_panel = CameraPanel(self, start_x + 698, start_y + 538, 9)
+
+        # Settings button panel
+        settings_panel_button = wx.Panel(self, pos=(1800, 468), size=(72, 72))
+        pic = wx.Bitmap("Settings.bmp", wx.BITMAP_TYPE_ANY)
+        self.settings_button = wx.BitmapButton(settings_panel_button, -1, pic)
+        self.Bind(wx.EVT_BUTTON, self.settings_button_pressed, self.settings_button)
+
+        # Presenting the name of the user to the screen
+        text_panel = wx.Panel(self, pos=(875, 20), size=(150, 30))
+        lbl = wx.StaticText(text_panel, -1, style=wx.ALIGN_CENTER)
+        box = wx.BoxSizer(wx.VERTICAL)
+
+        font = wx.Font(18, wx.ROMAN, wx.ITALIC, wx.NORMAL)
+        lbl.SetFont(font)
+        lbl.SetLabel('Hello {User}!')
+
+        box.Add(lbl, 0, wx.ALIGN_CENTER)
 
         # Locking the size of the frame
         self.SetMaxSize(wx.Size(1900, 1000))
         self.SetMinSize(wx.Size(1900, 1000))
 
+        # Setting the icon of the frame
         icon = wx.Icon()
         icon.CopyFromBitmap(wx.Bitmap("NoamCamLens.ico", wx.BITMAP_TYPE_ANY))
         self.SetIcon(icon)
 
-
+        # Showing and centring the frame in the screen
         self.Show()
         self.Centre()
 
+    def settings_button_pressed(self, e):
+        print("Settings button pressed")
+
+
 class SettingsFrame(wx.Frame):
 
-    def __init__(self):
-        wx.Frame.__init__(self, None, size=(500, 500), title="Settings")
+    def __init__(self, id_number):
+
+        self.id_number = id_number
+
+        super().__init__(None, size=(500, 500), title="Panel " + str(id_number) + " Settings")
+        wx.Dialog.__init__(self, None, title="Panel " + str(id_number) + " Settings")
         panel = wx.Panel(self, -1)
 
-        # pass info
+        icon = wx.Icon()
+        icon.CopyFromBitmap(wx.Bitmap("NoamCamLens.ico", wx.BITMAP_TYPE_ANY))
+        self.SetIcon(icon)
+
+        self.myDB = DB_Class.DB("myDB")
+
+        # mac info
         mac_sizer = wx.BoxSizer(wx.HORIZONTAL)
 
-        mac_label = wx.StaticText(self, label="Username:")
-        mac_sizer.Add(mac_label, 0, wx.ALL | wx.CENTER, 5)
-        self.user = wx.TextCtrl(self)
-        mac_sizer.Add(self.user, 0, wx.ALL, 5)
+        mac_lbl = wx.StaticText(self, label="MAC Address:")
+        mac_sizer.Add(mac_lbl, 0, wx.ALL | wx.CENTER, 5)
+        self.mac = wx.TextCtrl(self)
+        mac_sizer.Add(self.mac, 0, wx.ALL, 5)
 
+        # number info
+        number_sizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        number_lbl = wx.StaticText(self, label="Camera number:")
+        number_sizer.Add(number_lbl, 0, wx.ALL | wx.CENTER, 5)
+        self.number = wx.TextCtrl(self)
+        number_sizer.Add(self.number, 0, wx.ALL, 5)
+
+        # place info
+        place_sizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        place_lbl = wx.StaticText(self, label="Place:")
+        place_sizer.Add(place_lbl, 0, wx.ALL | wx.CENTER, 5)
+        self.place = wx.TextCtrl(self)
+        number_sizer.Add(self.place, 0, wx.ALL, 5)
+
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+        main_sizer.Add(mac_sizer, 0, wx.ALL, 5)
+        main_sizer.Add(number_sizer, 0, wx.ALL, 5)
+        main_sizer.Add(place_sizer, 0, wx.ALL, 5)
+
+        btn = wx.Button(self, label="Save")
+        btn.Bind(wx.EVT_BUTTON, self.save_settings)
+        main_sizer.Add(btn, 0, wx.ALL | wx.CENTER, 5)
+
+        self.SetSizer(main_sizer)
+
+        self.Bind(wx.EVT_CLOSE, self._when_closed)
+
+    def _when_closed(self, event):
+        self.Hide()
+
+
+
+    def save_settings(self, event):
+        mac_address = self.mac.GetValue()
+        number = self.number.GetValue()
+        place = self.place.GetValue()
+
+        print("The new saved settings are:" + mac_address + " " + number + " " + place)
 
 
 if __name__ == "__main__":
     app = wx.App(False)
-    frame = MainFrame()
+    frame = MainFrame(426, 98)
     app.MainLoop()
